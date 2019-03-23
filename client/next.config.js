975bfbb3a450a3bbeec79b62dfc49e76aca7142d
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-03-22 21:57:40 -0700

    Update packages

--- client/next.config.js
+++ client/next.config.js
@@ -1,40 +1,5 @@
-const path = require('path');
-const WorkboxPlugin = require('workbox-webpack-plugin');
+const withOffline = require('next-offline');
 
-/* eslint-disable */
-module.exports = {
-  webpack: (config, { dev }) => {
-    /**
-     * Install and Update our Service worker
-     * on our main entry file :)
-     * Reason: https://github.com/ooade/NextSimpleStarter/issues/32
-     */
-    const oldEntry = config.entry;
+const nextConfig = {};
 
-    config.entry = () =>
-      oldEntry().then((entry) => {
-        if (entry['main.js']) {
-          entry['main.js'].push(path.resolve(__dirname, 'utils/offline'));
-        }
-
-        return entry;
-      });
-
-    /* Enable only in Production */
-    if (!dev) {
-      // Service Worker
-      config.plugins.push(
-        new WorkboxPlugin.InjectManifest({
-          swSrc: path.join(__dirname, 'utils', 'sw.js'),
-          swDest: path.join(__dirname, '.next', 'sw.js'),
-          globDirectory: __dirname,
-          globPatterns: [
-            'static/**/*.{png,jpg,ico}', // Precache all static assets by default
-          ],
-        }),
-      );
-    }
-
-    return config;
-  },
-};
+module.exports = withOffline(nextConfig);
