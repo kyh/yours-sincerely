e7012eb1fe59dc3667b58b5529be8f3c35e438e9
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-03-22 21:44:43 -0700

    Remove unused components

--- client/components/steppers/src/StepButton.js
+++
@@ -1,6 +0,0 @@
-import { withStyles } from '@material-ui/core/styles';
-import StepButton from '@material-ui/core/StepButton';
-
-const styles = {};
-
-export default withStyles(styles)(StepButton);
--- client/components/steppers/src/StepContent.js
+++
@@ -1,6 +0,0 @@
-import { withStyles } from '@material-ui/core/styles';
-import StepContent from '@material-ui/core/StepContent';
-
-const styles = {};
-
-export default withStyles(styles)(StepContent);
