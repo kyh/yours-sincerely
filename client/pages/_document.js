f6e2a02d6c76a3113988ff0d6c0712ae05b86817
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-03-25 13:36:36 -0700

    Use meta component

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,33 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Main, NextScript } from 'next/document';
 import flush from 'styled-jsx/server';
+import { Meta } from '@components';
 
 class MyDocument extends Document {
   render() {
-    const { pageContext } = this.props;
-
     return (
       <html lang="en" dir="ltr">
-        <Head>
-          <meta charSet="utf-8" />
-          {/* Use minimum-scale=1 to enable GPU rasterization */}
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-          />
-          {/* PWA primary color */}
-          <meta
-            name="theme-color"
-            content={
-              pageContext ? pageContext.theme.palette.primary.main : null
-            }
-          />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
-          />
-        </Head>
+        <Meta />
         <body>
           <Main />
           <NextScript />
