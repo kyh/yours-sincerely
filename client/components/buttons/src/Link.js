0d13fb03274ce3cee97217b40b551dafb0c76220
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-03-24 21:24:27 -0700

    Add loading component, add link component

--- client/components/buttons/src/Link.js
+++ client/components/buttons/src/Link.js
@@ -1,4 +1,25 @@
 import React from 'react';
-import Link from '@material-ui/core/Link';
+import { withStyles } from '@material-ui/core/styles';
+import NextLink from 'next/link';
 
-export default React.memo(Link);
+const styles = (theme) => ({
+  root: {
+    fontFamily: theme.typography.fontFamily,
+    color: theme.palette.primary.main,
+    textDecoration: 'none',
+    backgroundColor: 'transparent',
+    borderBottom: `2px solid ${theme.palette.grey[300]}`,
+    paddingBottom: 1,
+    transition: 'color 0.2s linear, border-color 0.2s linear',
+    cursor: 'pointer',
+    '&:hover': {
+      borderColor: theme.palette.primary.main,
+    },
+  },
+});
+
+export default withStyles(styles)(({ children, classes, ...props }) => (
+  <NextLink {...props}>
+    <a className={classes.root}>{children}</a>
+  </NextLink>
+));
