8a12bf1d6bc8abb2dfedec4c897d84fc71e52f9a
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-03-27 23:06:21 -0700

    Update packages

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -8,7 +8,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import JssProvider from 'react-jss/lib/JssProvider';
 
 import getPageContext from '@client/utils/getPageContext';
-import { Meta } from '@components';
 
 class MyApp extends App {
   constructor() {
@@ -27,32 +26,30 @@ class MyApp extends App {
   render() {
     const { Component, pageProps, apolloClient } = this.props;
     return (
-      <ApolloProvider client={apolloClient}>
-        <Container>
-          <Meta />
-          {/* Wrap every page in Jss and Theme providers */}
-          <JssProvider
-            registry={this.pageContext.sheetsRegistry}
-            generateClassName={this.pageContext.generateClassName}
-          >
-            {/* MuiThemeProvider makes the theme available down the React
+      <Container>
+        {/* Wrap every page in Jss and Theme providers */}
+        <JssProvider
+          registry={this.pageContext.sheetsRegistry}
+          generateClassName={this.pageContext.generateClassName}
+        >
+          {/* MuiThemeProvider makes the theme available down the React
                 tree thanks to React context. */}
-            <MuiThemeProvider
-              theme={this.pageContext.theme}
-              sheetsManager={this.pageContext.sheetsManager}
-            >
-              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-              <CssBaseline />
-              {/* Pass pageContext to the _document though the renderPage enhancer
+          <MuiThemeProvider
+            theme={this.pageContext.theme}
+            sheetsManager={this.pageContext.sheetsManager}
+          >
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            {/* Pass pageContext to the _document though the renderPage enhancer
                   to render collected styles on server-side. */}
+            <ApolloProvider client={apolloClient}>
               <Component pageContext={this.pageContext} {...pageProps} />
-            </MuiThemeProvider>
-          </JssProvider>
-        </Container>
-      </ApolloProvider>
+            </ApolloProvider>
+          </MuiThemeProvider>
+        </JssProvider>
+      </Container>
     );
   }
 }
 
 export default withApolloClient(MyApp);
-
