c9d596ef2649d51c0f03a7779591a01705b0c2c4
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-03-27 23:27:17 -0700

    Remove unused files

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import App, { Container } from 'next/app';
+
 import { ApolloProvider } from 'react-apollo';
 import withApolloClient from '@client/utils/with-apollo-client';
 
