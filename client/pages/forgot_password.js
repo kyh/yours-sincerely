fba72d76cfae4a2c5d1b0e3aebb1eeb89a628abc
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-04-14 22:41:14 -0700

    Finish terms page

---
+++ client/pages/forgot_password.js
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Header, Text } from '@components';
+import { withStyles } from '@material-ui/core/styles';
+import Link from 'next/link';
+
+const styles = (theme) => ({
+  root: {
+    textAlign: 'center',
+    paddingTop: theme.spacing.unit * 20,
+  },
+});
+
+function ForgotPassword(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Header>Forgot Password</Header>
+    </div>
+  );
+}
+
+ForgotPassword.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ForgotPassword);
