ad9bd168a7321030ea8db15fdc93a1587913315f
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-04-17 22:03:20 -0700

    Update prisma, add permissions

--- server/db/generated/prisma-client/index.js
+++ server/db/generated/prisma-client/index.js
@@ -5,11 +5,11 @@ var typeDefs = require("./prisma-schema").typeDefs;
 
 var models = [
   {
-    name: "Post",
+    name: "User",
     embedded: false
   },
   {
-    name: "User",
+    name: "Post",
     embedded: false
   }
 ];
