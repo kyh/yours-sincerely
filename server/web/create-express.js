ca8dc32bb200cb6b7d62308dc10c562a6bec9b87
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-04-19 23:50:32 -0700

    Finish authenticaion

--- server/web/create-express.js
+++ server/web/create-express.js
@@ -7,14 +7,12 @@ const express = require('express');
 const avatarsMiddleware = require('adorable-avatars');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-const authMiddleware = require('@server/middlewares/authentication');
 
 function createExpress() {
   const app = express();
 
   app.use(helmet());
   app.use(cookieParser());
-  app.use(authMiddleware);
   app.use('/avatars', avatarsMiddleware);
 
   return app;
