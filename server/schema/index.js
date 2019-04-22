c48eb18bef0870330ea1438492c9a4f2da2427fd
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-04-21 17:57:30 -0700

    Update permissions middleware

--- server/schema/index.js
+++ server/schema/index.js
@@ -7,6 +7,6 @@ const permissions = require('@server/middlewares/permissions');
 
 const typeDefs = importSchema(path.resolve('server/schema/schema.graphql'));
 const schema = makeExecutableSchema({ typeDefs, resolvers });
-const schemaWithMiddleware = applyMiddleware(schema);
+const schemaWithMiddleware = applyMiddleware(schema, permissions);
 
 module.exports = schemaWithMiddleware;
