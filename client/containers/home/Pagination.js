b6873e2d6883934c5e7498bd068129c1770e4770
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-05-02 23:46:42 -0700

    Query for pages

--- client/containers/home/Pagination.js
+++ client/containers/home/Pagination.js
@@ -1,32 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 import { withStyles } from '@material-ui/core/styles';
 import { Link } from '@components';
 
 const styles = (theme) => ({});
 
+const PAGINATION_QUERY = gql`
+  query PAGINATION_QUERY {
+    postsConnection {
+      aggregate {
+        count
+      }
+    }
+  }
+`;
+
 function Pagination({ page }) {
   return (
-    <>
-      <Link
-        prefetch
-        href={{
-          pathname: '/',
-          query: { page: page - 1 },
-        }}
-      >
-        Previous
-      </Link>
-      <Link
-        prefetch
-        href={{
-          pathname: '/',
-          query: { page: page + 1 },
-        }}
-      >
-        Next
-      </Link>
-    </>
+    <Query query={PAGINATION_QUERY}>
+      {({ data, loading, error }) => {
+        if (loading || error) return null;
+        const { count } = data.postsConnection.aggregate;
+        const pages = Math.ceil(count / 1);
+        return (
+          <section>
+            {page > 1 && (
+              <Link
+                prefetch
+                href={{
+                  pathname: '/',
+                  query: { page: page - 1 },
+                }}
+              >
+                Next Page
+              </Link>
+            )}
+            {page < pages && (
+              <Link
+                prefetch
+                href={{
+                  pathname: '/',
+                  query: { page: page + 1 },
+                }}
+              >
+                Previous Page
+              </Link>
+            )}
+          </section>
+        );
+      }}
+    </Query>
   );
 }
 
