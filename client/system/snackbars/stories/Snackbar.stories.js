8365bb699fbbb5214d2bbc43f6335fda213386ee
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-05-03 19:37:25 -0700

    Remove unused components

--- client/system/snackbars/stories/Snackbar.stories.js
+++
@@ -1,9 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import React from 'react';
-import { Snackbar } from '@components';
-
-storiesOf('Components|Snackbar', module).add('Snackbar component', () => (
-  <div>
-    <Snackbar isOpen />
-  </div>
-));
