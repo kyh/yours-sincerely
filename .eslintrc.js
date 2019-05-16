17425ff91f54d91c2ce6e05c083cc16c544fb199
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-05-16 12:38:37 -0700

    Change up linter options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,13 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: ['airbnb', 'plugin:prettier/recommended'],
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier'],
   env: {
     browser: true,
     node: true,
     jest: true,
   },
   rules: {
+    'prettier/prettier': ['error'],
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     // Allow console logs in our codebase.
     'no-console': 'off',
