37578aa075c4be5990198be1e89c0019a9c53ad1
Xuan Yue <mintedfreshly@gmail.com>
2019-05-15 21:54:53 -0700

    added heroku cli download to docs (#4)

--- tools/heroku-mappings.js
+++ tools/heroku-mappings.js
@@ -1,6 +1,6 @@
 const DEPLOY_LOCATIONS = {
   dev: { herokuName: 'ys-development', value: 'dev' },
-  prod: { herokuName: 'ys-production', value: 'prod' },
+  prod: { herokuName: 'ys-app', value: 'prod' },
 };
 
 module.exports = DEPLOY_LOCATIONS;
