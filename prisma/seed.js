724064c1edbf7d20f1ea6efb8e31cc43e665bfa8
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-05-29 23:51:04 -0700

    Update YS local to use prisma demo database

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,24 +1,32 @@
-const { prisma } = require('../server/schema/generated/prisma-client');
+require('module-alias/register');
+const prisma = require('@server/services/db');
 
 async function main() {
-  await prisma.createUser({
-    username: 'kyh',
-    email: 'im.kaiyu@gmail.com',
-    name: 'Kaiyu',
-    password: '$2b$10$dqyYw5XovLjpmkYNiRDEWuwKaRAvLaG45fnXE5b3KTccKZcRPka2m', // "secret42"
-    posts: {
-      create: [
-        {
-          content:
-            'What does it mean to be a part of something like this? An endless, senseless story. Pen to paper, fingers to keyboard, words to scripture, thoughts to reflections, heart to heart. To have us all just spew out what’s on our minds at the moment -- kind of connects us all, just for a second, doesn’t it?',
+  try {
+    await prisma.mutation.createUser({
+      data: {
+        username: 'kyh',
+        email: 'im.kaiyu@gmail.com',
+        password:
+          '$2b$10$dqyYw5XovLjpmkYNiRDEWuwKaRAvLaG45fnXE5b3KTccKZcRPka2m', // "secret42",
+        permissions: { set: ['USER'] },
+        posts: {
+          create: [
+            {
+              content:
+                'What does it mean to be a part of something like this? An endless, senseless story. Pen to paper, fingers to keyboard, words to scripture, thoughts to reflections, heart to heart. To have us all just spew out what’s on our minds at the moment -- kind of connects us all, just for a second, doesn’t it?',
+            },
+            {
+              content:
+                'If you have trouble falling asleep, write down all the thoughts swimming around your head before getting into bed as a way to clear your mind.',
+            },
+          ],
         },
-        {
-          content:
-            'If you have trouble falling asleep, write down all the thoughts swimming around your head before getting into bed as a way to clear your mind.',
-        },
-      ],
-    },
-  });
+      },
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 main();
