5466a37e16d539682120a1f5ae250692b1ba6d73
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-10-12 14:07:08 +0700

    Upgrade forms to use react hooks

--- client/containers/LoginForm.js
+++ client/containers/LoginForm.js
@@ -1,125 +1,85 @@
-import React, { PureComponent } from 'react';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
+import React, { useState } from 'react';
+import useForm from 'react-hook-form';
 import { withStyles } from '@material-ui/core/styles';
-import { withApollo } from 'react-apollo';
-import { adopt } from 'react-adopt';
 
-import { FormField, Button, Snackbar } from '@components';
-import Login from '@client/containers/auth/Login';
-import redirect from '@client/utils/redirect';
+import { emailValidation } from '@utils/validation';
+import redirect from '@utils/redirect';
+
+import { TextField, Button, Snackbar } from '@components';
 
 const styles = (theme) => ({
   field: { marginBottom: theme.spacing(2) },
 });
 
-const validationSchema = Yup.object().shape({
-  email: Yup.string()
-    .email('That doesn’t seem right')
-    .required('That doesn’t seem right'),
-  password: Yup.string()
-    .min(6, 'Just a little longer')
-    .required('That doesn’t seem right'),
-});
+function LoginForm({ classes }) {
+  const { register, errors, handleSubmit } = useForm({
+    mode: 'onBlur',
+  });
+  const [loginState, setLoginState] = useState({
+    isLoading: false,
+    isErrorOpen: false,
+    errorMessage: '',
+  });
 
-const Composed = adopt({
-  // eslint-disable-next-line react/prop-types
-  login: ({ render, onError, onCompleted }) => (
-    <Login onError={onError} onCompleted={onCompleted}>
-      {(mutation, params) => render({ mutation, params })}
-    </Login>
-  ),
-  // eslint-disable-next-line react/prop-types
-  formik: ({ render, login }) => {
-    return (
-      <Formik
-        initialValues={{
-          email: '',
-          password: '',
-        }}
-        validationSchema={validationSchema}
-        onSubmit={(values) => login.mutation({ variables: values })}
-      >
-        {render}
-      </Formik>
-    );
-  },
-});
-
-class LoginForm extends PureComponent {
-  state = {
-    isErrorState: false,
-  };
-
-  closeErrorState = () => {
-    this.setState({ isErrorState: false });
-  };
-
-  onSubmitError = () => {
-    this.setState({ isErrorState: true });
-  };
-
-  onSubmitSuccess = () => {
-    const { client } = this.props;
-    // Force a reload of all the current queries now that the user is
-    // logged in
-    client.cache.reset().then(() => {
-      redirect({}, '/');
+  const onSubmit = (data) => {
+    console.log(data);
+    setLoginState({
+      isLoading: true,
+      isErrorOpen: false,
+      errorMessage: '',
     });
+    setTimeout(() => {
+      setLoginState({
+        isLoading: false,
+        isErrorOpen: true,
+        errorMessage: 'Incorrect email',
+      });
+    }, 1000);
   };
 
-  renderForm = ({ formik, login }) => {
-    const { loading, error } = login.params;
-    const { isErrorState } = this.state;
-    const { classes } = this.props;
-    return (
-      <>
-        <Snackbar
-          open={isErrorState}
-          variant="error"
-          message={error && error.message}
-          onClose={this.closeErrorState}
+  return (
+    <>
+      <Snackbar
+        open={loginState.isErrorOpen}
+        variant="error"
+        message={loginState.errorMessage}
+        onClose={() => setLoginState({ isErrorOpen: false, errorMessage: '' })}
+      />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          id="email"
+          name="email"
+          label="Email"
+          className={classes.field}
+          helperText={errors.email && "This email doesn't look right."}
+          error={!!errors.email}
+          inputRef={register({ required: true, pattern: emailValidation })}
         />
-        <form onSubmit={formik.handleSubmit} onReset={formik.handleReset}>
-          <FormField
-            id="email"
-            className={classes.field}
-            label="Email"
-            value={formik.values.email}
-            formikProps={formik}
-          />
-          <FormField
-            id="password"
-            className={classes.field}
-            label="Password"
-            value={formik.values.password}
-            formikProps={formik}
-            type="password"
-          />
-          <footer className={classes.footer}>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              size="large"
-              fullWidth
-              isLoading={loading}
-            >
-              Login
-            </Button>
-          </footer>
-        </form>
-      </>
-    );
-  };
-
-  render() {
-    return (
-      <Composed onError={this.onSubmitError} onCompleted={this.onSubmitSuccess}>
-        {this.renderForm}
-      </Composed>
-    );
-  }
+        <TextField
+          id="password"
+          name="password"
+          type="password"
+          label="Password"
+          className={classes.field}
+          helperText={errors.password && 'Password is required'}
+          error={!!errors.password}
+          inputRef={register({ required: true })}
+        />
+        <footer className={classes.footer}>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            size="large"
+            fullWidth
+            isLoading={loginState.isLoading}
+          >
+            Login
+          </Button>
+        </footer>
+      </form>
+    </>
+  );
 }
 
-export default withStyles(styles)(withApollo(LoginForm));
+export default withStyles(styles)(LoginForm);
--- client/containers/SignupForm.js
+++ client/containers/SignupForm.js
@@ -1,134 +1,85 @@
-import React, { PureComponent } from 'react';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
+import React, { useState } from 'react';
+import useForm from 'react-hook-form';
 import { withStyles } from '@material-ui/core/styles';
-import { withApollo } from 'react-apollo';
-import { adopt } from 'react-adopt';
 
+import { emailValidation } from '@utils/validation';
 import redirect from '@utils/redirect';
 
-import Signup from '@containers/auth/Signup';
-import { FormField, Button, Snackbar } from '@components';
+import { TextField, Button, Snackbar } from '@components';
 
 const styles = (theme) => ({
   field: { marginBottom: theme.spacing(2) },
 });
 
-const validationSchema = Yup.object().shape({
-  username: Yup.string().required('This will be how people find you'),
-  email: Yup.string()
-    .email('That doesn’t seem right')
-    .required('This is how you log in'),
-  password: Yup.string()
-    .min(6, 'Just a little longer')
-    .required('Your password is required'),
-});
+function SignupForm({ classes }) {
+  const { register, errors, handleSubmit } = useForm({
+    mode: 'onBlur',
+  });
+  const [signupState, setSignupState] = useState({
+    isLoading: false,
+    isErrorOpen: false,
+    errorMessage: '',
+  });
 
-const Composed = adopt({
-  // eslint-disable-next-line react/prop-types
-  signup: ({ render, onError, onCompleted }) => (
-    <Signup onError={onError} onCompleted={onCompleted}>
-      {(mutation, params) => render({ mutation, params })}
-    </Signup>
-  ),
-  // eslint-disable-next-line react/prop-types
-  formik: ({ render, signup }) => {
-    return (
-      <Formik
-        initialValues={{
-          username: '',
-          email: '',
-          password: '',
-        }}
-        validationSchema={validationSchema}
-        onSubmit={(values) => signup.mutation({ variables: values })}
-      >
-        {render}
-      </Formik>
-    );
-  },
-});
-
-class SignupForm extends PureComponent {
-  state = {
-    isErrorState: false,
+  const onSubmit = (data) => {
+    console.log(data);
+    setSignupState({
+      isLoading: true,
+      isErrorOpen: false,
+      errorMessage: '',
+    });
+    setTimeout(() => {
+      setSignupState({
+        isLoading: false,
+        isErrorOpen: true,
+        errorMessage: 'Incorrect email',
+      });
+    }, 1000);
   };
 
-  closeErrorState = () => {
-    this.setState({ isErrorState: false });
-  };
-
-  onSubmitError = () => {
-    this.setState({ isErrorState: true });
-  };
-
-  onSubmitSuccess = async () => {
-    // Force a reload of all the current queries now that the user is
-    // logged in.
-    const { client } = this.props;
-    await client.resetStore();
-    redirect({}, '/');
-  };
-
-  renderForm = ({ formik, signup }) => {
-    const { loading, error } = signup.params;
-    const { isErrorState } = this.state;
-    const { classes } = this.props;
-    return (
-      <>
-        <Snackbar
-          open={isErrorState}
-          variant="error"
-          message={error && error.message}
-          onClose={this.closeErrorState}
+  return (
+    <>
+      <Snackbar
+        open={signupState.isErrorOpen}
+        variant="error"
+        message={signupState.errorMessage}
+        onClose={() => setSignupState({ isErrorOpen: false, errorMessage: '' })}
+      />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          id="email"
+          name="email"
+          label="Email"
+          className={classes.field}
+          helperText={errors.email && "This email doesn't look right."}
+          error={!!errors.email}
+          inputRef={register({ required: true, pattern: emailValidation })}
         />
-        <form onSubmit={formik.handleSubmit} onReset={formik.handleReset}>
-          <FormField
-            id="username"
-            className={classes.field}
-            label="Username"
-            value={formik.values.username}
-            formikProps={formik}
-          />
-          <FormField
-            id="email"
-            className={classes.field}
-            label="Email"
-            value={formik.values.email}
-            formikProps={formik}
-          />
-          <FormField
-            id="password"
-            className={classes.field}
-            label="Password"
-            value={formik.values.password}
-            formikProps={formik}
-            type="password"
-          />
-          <footer className={classes.footer}>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              size="large"
-              fullWidth
-              isLoading={loading}
-            >
-              Sign Up
-            </Button>
-          </footer>
-        </form>
-      </>
-    );
-  };
-
-  render() {
-    return (
-      <Composed onError={this.onSubmitError} onCompleted={this.onSubmitSuccess}>
-        {this.renderForm}
-      </Composed>
-    );
-  }
+        <TextField
+          id="password"
+          name="password"
+          type="password"
+          label="Password"
+          className={classes.field}
+          helperText={errors.password && 'Password is required'}
+          error={!!errors.password}
+          inputRef={register({ required: true })}
+        />
+        <footer className={classes.footer}>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            size="large"
+            fullWidth
+            isLoading={signupState.isLoading}
+          >
+            Sign Up
+          </Button>
+        </footer>
+      </form>
+    </>
+  );
 }
 
-export default withStyles(styles)(withApollo(SignupForm));
+export default withStyles(styles)(SignupForm);
