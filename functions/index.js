999c93097445dbc31be95eb1a0598dde286c3f18
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-04-29 23:57:29 -0700

    Add ability to flag posts

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const functions = require("firebase-functions");
 
 admin.initializeApp(functions.config().firebase);
 
+const postFlags = require("./lib/postFlags");
 const postLikes = require("./lib/postLikes");
 const search = require("./lib/search");
 const subscriptions = require("./lib/subscriptions");
@@ -18,3 +19,7 @@ exports.updateStripeSubscription = functions.firestore
 exports.updatePostLikeCount = functions.firestore
   .document("postLikes/{postLikeId}")
   .onWrite(postLikes.updatePostLikeCount);
+
+exports.updatePostLikeCount = functions.firestore
+  .document("postFlags/{postFlagId}")
+  .onWrite(postFlags.updatePostFlag);
---
+++ src/features/posts/components/FlagButton.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Modal } from "components/Modal";
+import { Icon } from "components/Icon";
+import { flagPost } from "features/posts/actions/postActions";
+
+export const FlagButton = ({ postId }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      <IconContainer onClick={() => setIsOpen(true)}>
+        <Icon icon="more" color="grey" />
+      </IconContainer>
+      <Modal
+        open={isOpen}
+        onRequestClose={() => setIsOpen(false)}
+        maxWidth={400}
+        closeButton
+      >
+        <ModalContent>
+          <ModalButton
+            as="a"
+            href={`mailto:im.kaiyu@gmail.com?subject=Report Post: ${postId}`}
+          >
+            Report...
+          </ModalButton>
+          <ModalButton type="button" onClick={() => flagPost(postId)}>
+            Mark as inappropriate
+          </ModalButton>
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+};
+
+const IconContainer = styled.button`
+  width: 45px;
+  height: 45px;
+  border-radius: 100%;
+  transition: 0.2s ease;
+  &:hover {
+    background-color: ${({ theme }) => theme.ui.border};
+  }
+`;
+
+const ModalContent = styled.section`
+  padding: ${({ theme }) =>
+    `${theme.spacings(10)} ${theme.spacings(5)} ${theme.spacings(5)}`};
+`;
+
+const ModalButton = styled.button`
+  text-align: center;
+  display: block;
+  width: 100%;
+  padding: ${({ theme }) => theme.spacings(5)};
+  border-bottom: 1px solid ${({ theme }) => theme.ui.border};
+  transition: all 0.2s ease;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.ui.background};
+  }
+`;
