28eaa2746d8718f420d6779eb85097d0a18127a7
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-05-01 20:31:13 -0700

    Update flag

--- functions/lib/postFlags.js
+++ functions/lib/postFlags.js
@@ -2,11 +2,15 @@ const admin = require("firebase-admin");
 
 // update _flagged on a post
 exports.updatePostFlag = (change, context) => {
-  const postId = change.after.exists
-    ? change.after.data().postId
-    : change.before.data().postId;
+  const data = change.after.exists ? change.after.data() : change.before.data();
+  const { postId, createdBy } = data.postId;
 
-  return admin.firestore().collection("posts").doc(postId).update({
-    _flagged: true,
-  });
+  return Promise.all([
+    admin.firestore().collection("posts").doc(postId).update({
+      _flagged: true,
+    }),
+    admin.auth().updateUser(createdBy, {
+      _flagged: true,
+    }),
+  ]);
 };
