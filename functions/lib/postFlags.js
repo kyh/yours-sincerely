e88397621f748ab039bf22c874d45d14c8db7816
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-05-01 20:36:04 -0700

    Fix data

--- functions/lib/postFlags.js
+++ functions/lib/postFlags.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 // update _flagged on a post
 exports.updatePostFlag = (change, context) => {
   const data = change.after.exists ? change.after.data() : change.before.data();
-  const { postId, createdBy } = data.postId;
+  const { postId, createdBy } = data;
 
   return Promise.all([
     admin.firestore().collection("posts").doc(postId).update({
