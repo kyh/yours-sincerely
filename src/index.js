cc9648f9ecf261d1ce9dcc670de5cf7d4ba65c84
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-06-20 22:45:35 -0700

    Add analytics

--- src/index.js
+++ src/index.js
@@ -14,8 +14,13 @@ const dbConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 firebase.initializeApp(dbConfig);
+firebase.analytics();
 
 // resize listeners
 const onResize = () => {
