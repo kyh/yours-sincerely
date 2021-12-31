c43ef2673bcc5856034116d7dff5759c2d30310a
Kaiyu Hsu <im.kaiyu@gmail.com>
2021-12-31 01:50:13 -0800

    add logout

--- remix/app/lib/user/ui/Profile.tsx
+++ remix/app/lib/user/ui/Profile.tsx
@@ -1,107 +1,134 @@
-import { useState } from "react";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import styled from "styled-components";
-import { Input } from "components/Input";
-import { Spinner } from "components/Spinner";
-import { ProfileDetails } from "components/ProfileDetails";
-import { useAuth } from "actions/auth";
-import { isIOS } from "util/platform";
+import { Form, useLoaderData } from "remix";
+import { Session } from "~/lib/auth/data/authSchema";
+import { isIOS } from "~/lib/core/util/platform";
+
+type LoaderData = {
+  user: Session["user"];
+};
 
 export const Profile = () => {
-  const router = useRouter();
-  const { user, updateProfile, signout } = useAuth();
-  const [isLoading, setIsLoading] = useState(false);
-
-  const changeName = async (event) => {
-    event.preventDefault();
-    const name = event.target.value;
-    if (name) {
-      setIsLoading(true);
-      await updateProfile({ displayName: name });
-      setIsLoading(false);
-    }
-  };
-
-  const logout = async () => {
-    await signout();
-    router.push(`/`);
-  };
-
+  const { user } = useLoaderData<LoaderData>();
+  console.log(user);
   return (
-    <ProfileDetails>
-      <Image
+    <section className="sm:flex">
+      <img
+        className="m-auto sm:w-1/2"
         src="/assets/dancing.svg"
-        alt="Logged in"
+        alt="Not logged in"
         width={300}
         height={225}
       />
-      <ProfileForm>
-        <InputContainer>
-          <NameInput
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Bojack the horse"
-            disabled={isLoading}
-            defaultValue={user.displayName}
-            onBlur={changeName}
-            required
-          />
-          <InputSpinner loading={isLoading} />
-        </InputContainer>
-        {!isIOS() && (
-          <button
-            type="button"
-            className="logout"
-            disabled={isLoading}
-            onClick={logout}
-          >
-            Log out
-          </button>
-        )}
-      </ProfileForm>
-    </ProfileDetails>
+      <div className="sm:w-1/2">
+        <Form action="/auth/logout" method="post">
+          <button type="submit">Logout</button>
+        </Form>
+      </div>
+    </section>
   );
 };
 
-const InputContainer = styled.div`
-  position: relative;
-`;
+// import { useState } from "react";
+// import styled from "styled-components";
+// import { Input } from "components/Input";
+// import { Spinner } from "components/Spinner";
+// import { ProfileDetails } from "components/ProfileDetails";
+// import { useAuth } from "actions/auth";
+// import { isIOS } from "util/platform";
 
-const InputSpinner = styled(Spinner)`
-  position: absolute;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-`;
+// export const Profile = () => {
+//   const router = useRouter();
+//   const { user, updateProfile, signout } = useAuth();
+//   const [isLoading, setIsLoading] = useState(false);
 
-const NameInput = styled(Input)`
-  font-size: 2rem;
-  font-weight: 600;
-  max-width: 100%;
-`;
+//   const changeName = async (event) => {
+//     event.preventDefault();
+//     const name = event.target.value;
+//     if (name) {
+//       setIsLoading(true);
+//       await updateProfile({ displayName: name });
+//       setIsLoading(false);
+//     }
+//   };
 
-const ProfileForm = styled.form`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
+//   const logout = async () => {
+//     await signout();
+//     router.push(`/`);
+//   };
 
-  .submit {
-    margin-top: ${({ theme }) => theme.spacings(3)};
-  }
-  .logout {
-    display: inline-block;
-    margin: auto auto 0;
-    color: ${({ theme }) => theme.colors.red};
-    padding: ${({ theme }) => `${theme.spacings(3)} ${theme.spacings(6)}`};
-    border-radius: 4px;
-    transition: background 0.23s ease;
-    &:hover {
-      background: ${({ theme }) => theme.colors.backgroundRed};
-    }
-    ${({ theme }) => theme.breakpoints.sm`
-      margin: ${({ theme }) => theme.spacings(10)} auto;
-    `}
-  }
-`;
+//   return (
+//     <ProfileDetails>
+//       <Image
+//         src="/assets/dancing.svg"
+//         alt="Logged in"
+//         width={300}
+//         height={225}
+//       />
+//       <ProfileForm>
+//         <InputContainer>
+//           <NameInput
+//             type="text"
+//             id="name"
+//             name="name"
+//             placeholder="Bojack the horse"
+//             disabled={isLoading}
+//             defaultValue={user.displayName}
+//             onBlur={changeName}
+//             required
+//           />
+//           <InputSpinner loading={isLoading} />
+//         </InputContainer>
+//         {!isIOS() && (
+//           <button
+//             type="button"
+//             className="logout"
+//             disabled={isLoading}
+//             onClick={logout}
+//           >
+//             Log out
+//           </button>
+//         )}
+//       </ProfileForm>
+//     </ProfileDetails>
+//   );
+// };
+
+// const InputContainer = styled.div`
+//   position: relative;
+// `;
+
+// const InputSpinner = styled(Spinner)`
+//   position: absolute;
+//   right: 0;
+//   top: 50%;
+//   transform: translateY(-50%);
+// `;
+
+// const NameInput = styled(Input)`
+//   font-size: 2rem;
+//   font-weight: 600;
+//   max-width: 100%;
+// `;
+
+// const ProfileForm = styled.form`
+//   width: 100%;
+//   display: flex;
+//   flex-direction: column;
+
+//   .submit {
+//     margin-top: ${({ theme }) => theme.spacings(3)};
+//   }
+//   .logout {
+//     display: inline-block;
+//     margin: auto auto 0;
+//     color: ${({ theme }) => theme.colors.red};
+//     padding: ${({ theme }) => `${theme.spacings(3)} ${theme.spacings(6)}`};
+//     border-radius: 4px;
+//     transition: background 0.23s ease;
+//     &:hover {
+//       background: ${({ theme }) => theme.colors.backgroundRed};
+//     }
+//     ${({ theme }) => theme.breakpoints.sm`
+//       margin: ${({ theme }) => theme.spacings(10)} auto;
+//     `}
+//   }
+// `;
--- remix/app/routes/__app/profile.tsx
+++ remix/app/routes/__app/profile.tsx
@@ -1,9 +1,30 @@
+import { LoaderFunction, useLoaderData } from "remix";
+import { authenticator } from "~/lib/auth/server/middleware/auth.server";
+import { Session } from "~/lib/auth/data/authSchema";
+import { Profile } from "~/lib/user/ui/Profile";
 import { NoProfile } from "~/lib/user/ui/NoProfile";
 
-const Page = () => (
-  <main className="pt-5">
-    <NoProfile />
-  </main>
-);
+type LoaderData = {
+  user: Session["user"];
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await authenticator.isAuthenticated(request);
+  const data: LoaderData = {
+    user,
+  };
+
+  return data;
+};
+
+const Page = () => {
+  const { user } = useLoaderData<LoaderData>();
+  return (
+    <main className="pt-5">
+      {!user && <NoProfile />}
+      {!!user && <Profile />}
+    </main>
+  );
+};
 
 export default Page;
