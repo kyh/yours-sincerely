ba1d0c2661965436efb36fd75fd15fa6ad390af4
Kaiyu Hsu <im.kaiyu@gmail.com>
2022-01-03 14:43:52 -0800

    migrate to remix

--- web/src/components/PostContent.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const PostContent = styled.p`
-  font-size: ${({ theme }) => theme.typography.post.fontSize};
-  line-height: ${({ theme }) => theme.typography.post.lineHeight};
-  white-space: pre-wrap;
-  margin-bottom: ${({ theme }) => theme.spacings(3)};
-`;
--- web/src/components/Text.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const Text = styled.p`
-  line-height: 1.6;
-`;
