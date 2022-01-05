0b62ac123a68484e15b2b335079ef0e848d1492d
Kaiyu Hsu <im.kaiyu@gmail.com>
2022-01-05 02:06:27 -0800

    add in db migration script

--- app/routes/__app/auth/logout.tsx
+++ app/routes/__app/auth/logout.tsx
@@ -5,14 +5,14 @@ import {
   destroySession,
 } from "~/lib/auth/server/middleware/session.server";
 
+export const loader: LoaderFunction = () => {
+  throw new Response("", { status: 404 });
+};
+
 export const action: ActionFunction = async ({ request }) => {
   return redirect("/", {
     headers: {
       "Set-Cookie": await destroySession(await getSession(request)),
     },
   });
-};
-
-export const loader: LoaderFunction = () => {
-  throw new Response("", { status: 404 });
 };
---
+++ scripts/migrate-prod-db.ts
@@ -0,0 +1,8 @@
+import concurrently from "concurrently";
+
+const dbUrl = process.env.DATABASE_MIGRATION_URL;
+
+concurrently([
+  `pscale connect yours-sincerely shadow --port 3310`,
+  `DATABASE_URL=${dbUrl} npx prisma db push`,
+]);
