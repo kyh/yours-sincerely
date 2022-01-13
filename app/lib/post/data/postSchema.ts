f860d114d5276dc4f961fb2176c4ffd8c9b60a9a
Kaiyu Hsu <im.kaiyu@gmail.com>
2022-01-13 00:40:34 -0800

    clean up thrown errors

--- app/lib/post/data/postSchema.ts
+++ app/lib/post/data/postSchema.ts
@@ -9,3 +9,7 @@ export type Post = Partial<PrismaPost> & {
 };
 
 export const POST_EXPIRY_DAYS_AGO = 21;
+
+export const isPostContentValid = (content?: string): content is string => {
+  return !!content && content.length > 10;
+};
