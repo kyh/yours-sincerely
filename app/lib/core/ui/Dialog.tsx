a5d02999f5066a99aedf8b62a9d5a4847d960e4f
Kaiyu Hsu <im.kaiyu@gmail.com>
2022-12-26 19:35:34 -0800

    fix dialog not dismissing

--- app/lib/core/ui/Dialog.tsx
+++ app/lib/core/ui/Dialog.tsx
@@ -15,39 +15,37 @@ export const Dialog = ({
   children,
 }: Props) => (
   <Transition appear show={isOpen} as={Fragment}>
-    <HeadlessDialog
-      as="div"
-      className="fixed inset-0 z-10 overflow-y-auto"
-      onClose={handleClose}
-    >
-      <div className="min-h-screen px-4 text-center">
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <HeadlessDialog.Overlay className="fixed inset-0 bg-secondary opacity-70" />
-        </Transition.Child>
+    <HeadlessDialog as="div" className="relative z-10" onClose={handleClose}>
+      <Transition.Child
+        as={Fragment}
+        enter="ease-out duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="ease-in duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="fixed inset-0 bg-black bg-opacity-25" />
+      </Transition.Child>
 
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <div
-            className={`inline-block w-full max-w-md p-6 my-10 overflow-hidden text-center align-middle transition-all transform bg-white shadow-xl rounded-2xl dark:bg-slate-900 ${className}`}
+      <div className="fixed inset-0 overflow-y-auto">
+        <div className="flex min-h-full items-start justify-center p-4 text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
           >
-            {children}
-          </div>
-        </Transition.Child>
+            <HeadlessDialog.Panel
+              className={`inline-block w-full max-w-md p-6 my-10 overflow-hidden text-center align-middle transition-all transform bg-white shadow-xl rounded-2xl dark:bg-slate-900 ${className}`}
+            >
+              {children}
+            </HeadlessDialog.Panel>
+          </Transition.Child>
+        </div>
       </div>
     </HeadlessDialog>
   </Transition>
