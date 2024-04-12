4472f1e3f0cdd1c018bf2c3973530ecc0866b7a5
Kaiyu Hsu <im.kaiyu@gmail.com>
2024-04-11 17:42:57 -0700

    Initial commit

---
+++ apps/nextjs/next.config.js
@@ -0,0 +1,24 @@
+import { recmaPlugins } from "@init/mdx/plugins/recma.mjs";
+import { rehypePlugins } from "@init/mdx/plugins/rehype.mjs";
+import { remarkPlugins } from "@init/mdx/plugins/remark.mjs";
+import createMDX from "@next/mdx";
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins,
+    rehypePlugins,
+    recmaPlugins,
+  },
+});
+
+/** @type {import("next").NextConfig} */
+const config = {
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: ["@init/api", "@init/auth", "@init/db", "@init/ui"],
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
+
+export default withMDX(config);
---
+++ apps/nextjs/src/app/layout.tsx
@@ -0,0 +1,67 @@
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@init/ui/theme";
+import { Toaster } from "@init/ui/toast";
+import { cn } from "@init/ui/utils";
+
+import { TRPCReactProvider } from "@/trpc/react";
+
+import "./globals.css";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(
+    process.env.VERCEL_ENV === "production"
+      ? "https://init.kyh.io"
+      : "http://localhost:3000",
+  ),
+  title: {
+    template: "%s - Init",
+    default: "Init",
+  },
+  description:
+    "A comprehensive boilerplate to build, launch, and scale your next project",
+  openGraph: {
+    title: "Init",
+    description:
+      "A comprehensive boilerplate to build, launch, and scale your next project",
+    url: "https://github.com/kyh/init",
+    siteName: "Init",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@kaiyuhsu",
+    creator: "@kaiyuhsu",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+const fontSans = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "bg-background font-sans text-foreground antialiased",
+          fontSans.variable,
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+};
+
+export default Layout;
