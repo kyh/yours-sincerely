8fb42dfd23d934d0ba31a9163cb54ce473b120bf
Kaiyu Hsu <im.kaiyu@gmail.com>
2024-08-20 17:43:53 +0100

    updates

--- packages/db/src/database.types.ts
+++ packages/db/src/database.types.ts
@@ -171,6 +171,13 @@ export type Database = {
             referencedColumns: ["id"]
           },
           {
+            foreignKeyName: "Flag_postId_fkey"
+            columns: ["postId"]
+            isOneToOne: false
+            referencedRelation: "publicFeed"
+            referencedColumns: ["id"]
+          },
+          {
             foreignKeyName: "Flag_userId_fkey"
             columns: ["userId"]
             isOneToOne: false
@@ -204,6 +211,13 @@ export type Database = {
             columns: ["postId"]
             isOneToOne: false
             referencedRelation: "Post"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "Like_postId_fkey"
+            columns: ["postId"]
+            isOneToOne: false
+            referencedRelation: "publicFeed"
             referencedColumns: ["id"]
           },
           {
@@ -296,6 +310,13 @@ export type Database = {
             columns: ["parentId"]
             isOneToOne: false
             referencedRelation: "Post"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "Post_parentId_fkey"
+            columns: ["parentId"]
+            isOneToOne: false
+            referencedRelation: "publicFeed"
             referencedColumns: ["id"]
           },
           {
@@ -407,7 +428,26 @@ export type Database = {
       }
     }
     Views: {
-      [_ in never]: never
+      publicFeed: {
+        Row: {
+          author_id: string | null
+          commentCount: number | null
+          content: string | null
+          createdAt: string | null
+          id: string | null
+          isLiked: boolean | null
+          likeCount: number | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "Post_userId_fkey"
+            columns: ["author_id"]
+            isOneToOne: false
+            referencedRelation: "User"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
     }
     Functions: {
       getRandomPrompt: {
