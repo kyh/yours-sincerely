9e848551fa15b2c1895181940356962282306ee2
Kaiyu Hsu <im.kaiyu@gmail.com>
2024-08-21 03:00:06 -0700

    move some files, fix client hydration

--- apps/nextjs/src/app/(app)/posts/_components/comment-feed.tsx
+++ apps/nextjs/src/app/(app)/posts/_components/comment-feed.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { formatPost } from "@init/api/post/post-utils";
 import { Button } from "@init/ui/button";
 import { toast } from "@init/ui/toast";
 import readingTime from "reading-time";
 
-import type { RouterOutputs } from "@init/api";
 import { api } from "@/trpc/react";
 import { BackButton } from "./back-button";
 import { CommentContent } from "./comment-content";
@@ -13,18 +11,17 @@ import { PostContent } from "./post-content";
 
 type Props = {
   pid: string;
-  user: RouterOutputs["account"]["me"];
 };
 
-export const CommentFeed = ({ pid, user }: Props) => {
+export const CommentFeed = ({ pid }: Props) => {
+  const [user] = api.user.me.useSuspenseQuery();
   const [post] = api.post.getPost.useSuspenseQuery({ id: pid });
-  const utils = api.useUtils();
+
   const { mutate, isPending } = api.post.createPost.useMutation({
-    onSuccess: async () => {
+    onSuccess: () => {
       toast("Your comment has been added");
-      await utils.post.getPost.invalidate();
     },
-    onError: async (err) => {
+    onError: (err) => {
       toast("You got some errors");
       console.error(err);
     },
