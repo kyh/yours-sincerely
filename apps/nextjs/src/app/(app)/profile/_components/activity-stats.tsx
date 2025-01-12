c6388e24262e24528bb9e71891a8bdfece68a2a4
Kaiyu Hsu <im.kaiyu@gmail.com>
2025-01-11 17:19:56 -0800

    update tooltip ui

--- apps/nextjs/src/app/(app)/profile/_components/activity-stats.tsx
+++ apps/nextjs/src/app/(app)/profile/_components/activity-stats.tsx
@@ -12,27 +12,23 @@ export const ActivityStats = ({ data, stack }: Props) => {
   return (
     <dl className={`grid gap-2 ${stack ? "grid-cols-2" : "grid-cols-4"}`}>
       <div className="flex flex-col gap-1 text-center">
-        <dt className="order-2 text-xs not-italic text-muted-foreground">
-          🔥 Current Streak
+        <dt className="order-2 text-xs text-muted-foreground">
+          Current Streak
         </dt>
         <dd className="order-1 text-lg font-extrabold">{data.currentStreak}</dd>
       </div>
       <div className="flex flex-col gap-1 text-center">
-        <dt className="order-2 text-xs not-italic text-muted-foreground">
-          ✍️ Longest Streak
+        <dt className="order-2 text-xs text-muted-foreground">
+          Longest Streak
         </dt>
         <dd className="order-1 text-lg font-extrabold">{data.longestStreak}</dd>
       </div>
       <div className="flex flex-col gap-1 text-center">
-        <dt className="order-2 text-xs not-italic text-muted-foreground">
-          💯 Total Posts
-        </dt>
+        <dt className="order-2 text-xs text-muted-foreground">Total Posts</dt>
         <dd className="order-1 text-lg font-extrabold">{data.posts}</dd>
       </div>
       <div className="flex flex-col gap-1 text-center">
-        <dt className="order-2 text-xs not-italic text-muted-foreground">
-          💖 Total Likes
-        </dt>
+        <dt className="order-2 text-xs text-muted-foreground">Total Likes</dt>
         <dd className="order-1 text-lg font-extrabold">{data.likes}</dd>
       </div>
     </dl>
