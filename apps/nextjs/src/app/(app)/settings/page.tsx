a4bc7eadefbb6b6113be71d7faa360c2ea118b24
Kaiyu Hsu <im.kaiyu@gmail.com>
2025-01-19 14:44:44 -0800

    fix dynamic pages

--- apps/nextjs/src/app/(app)/settings/page.tsx
+++ apps/nextjs/src/app/(app)/settings/page.tsx
@@ -1,7 +1,15 @@
+import type { Metadata } from "next";
+
 import { PageContent, PageHeader } from "@/components/layout/page-layout";
 import { HydrateClient } from "@/trpc/server";
 import { AppearanceForm } from "./_components/appearance-form";
 import { SettingsForm } from "./_components/settings-form";
+
+export const metadata: Metadata = {
+  title: "Settings",
+};
+
+export const dynamic = "force-dynamic";
 
 const Page = () => {
   return (
