c48f2c5bc4c78cb733421f620fe436a074dd70a2
Kaiyu Hsu <im.kaiyu@gmail.com>
2025-02-02 14:57:20 -0800

    fix share

--- apps/nextjs/src/app/(app)/posts/_components/comment-button.tsx
+++ apps/nextjs/src/app/(app)/posts/_components/comment-button.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { MessageCircleIcon } from "lucide-react";
 
 import type { RouterOutputs } from "@init/api";
 
@@ -12,20 +13,7 @@ export const CommentButton = ({ post }: Props) => {
       className="relative flex h-8 items-center gap-1.5 rounded-lg p-2 transition hover:bg-accent"
       href={`/posts/${post.id}`}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="-translate-y-px"
-      >
-        <path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z" />
-      </svg>
+      <MessageCircleIcon className="size-4" />
       <span className="min-w-[0.75rem]">
         {post.commentCount}
         <span className="sr-only"> comments, click to see details</span>
